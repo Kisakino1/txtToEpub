35b4db90f9d78c4d582ed16bb950de427879286b
Kisaki <476586055@qq.com>
2025-01-06 10:27:14 +0800

    Initial commit

---
+++ utils/检查文件编码.py
@@ -0,0 +1,35 @@
+import chardet
+
+
+def detect_file_encoding(file_path):
+    # 读取文件的前10000个字节用于检测编码
+    with open(file_path, 'rb') as file:
+        raw_data = file.read(10000)
+
+    # 使用 chardet 检测文件编码
+    result = chardet.detect(raw_data)
+
+    # 获取编码格式和置信度
+    encoding = result.get('encoding')
+    confidence = result.get('confidence')
+
+    return encoding, confidence
+
+
+# 要检测的文件路径
+#file_path = "D:\Documents\WeChat Files\wxid_q8r1q7xxjv8h22\FileStorage\File\\2024-09\《天可汗》⊙校对版全本+隐藏6章+msmw一修.txt"
+file_path = "D:\Documents\WeChat Files\wxid_q8r1q7xxjv8h22\FileStorage\File\\2024-09\(NEW)《天可汗》⊙校对版全本+隐藏6章+msmw一修.txt"
+
+
+# 检测文件编码
+try:
+    encoding, confidence = detect_file_encoding(file_path)
+    if encoding:
+        print(f"文件编码格式：{encoding}")
+        print(f"检测置信度：{confidence * 100:.2f}%")
+    else:
+        print("无法检测文件的编码格式。")
+except FileNotFoundError:
+    print(f"文件未找到: {file_path}")
+except Exception as e:
+    print(f"发生异常: {e}")
